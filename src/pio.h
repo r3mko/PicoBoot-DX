df28adb18e1da81831fa8169185078d7c8ec9606
Remko Kleinjan <r.a.kleinjan@gmail.com>
2025-05-24 20:25:31 +0200

    Update prototype in pio.h

--- src/pio.h
+++ src/pio.h
@@ -10,4 +10,4 @@
 
 void on_transfer_program_init(PIO pio, uint sm, uint offset, uint clk_pin, uint cs_pin, uint out_pin);
 void clocked_output_program_init(PIO pio, uint sm, uint offset, uint data_pin, uint clk_pin, uint cs_pin);
-void pio_load_and_start(PIO pio, uint sm, uint32_t count, uint dest); 
+void pio_prepare_transfer(PIO pio, uint sm, uint32_t count, uint dest); 
