0b38bd97ced0f1002d694302aadfb800208b6f2f
Remko Kleinjan <r.a.kleinjan@gmail.com>
2025-06-14 22:34:05 +0200

    use correct prime length for scrambler

--- tools/process_ipl.py
+++ tools/process_ipl.py
@@ -172,9 +172,8 @@ def main():
 
     # Create: 0x720-byte header + image
     # 224 CS pulses * 8 = 1792 = 0x700
-    # 32 pipeline bytes = 0x20
-    #hdr_img = bytearray(0x720) + img
-    hdr_img = bytearray(0x20) + img
+    # 32 pipeline flush bytes = 0x20
+    hdr_img = bytearray(0x720) + img
 
     # Align: image size to the next 4-byte boundary if needed
     align_size = 4 # bytes
@@ -184,9 +183,8 @@ def main():
         print(f"Image needs to be aligned to {new_size} bytes ({chunks}x{align_size}B)")
         hdr_img += bytearray(new_size - size)
 
-    # Scramble: remove the first 0x720 bytes (header) after
-    #scrambled_img = scramble(hdr_img)[0x700:]
-    scrambled_img = scramble(hdr_img)
+    # Scramble: remove the first 0x700 bytes (224 CS) after, but keep 0x20 bytes (pipeline flush)
+    scrambled_img = scramble(hdr_img)[0x700:]
 
     print(f"Output (scrambled) binary size: {len(scrambled_img)} bytes ({len(scrambled_img) // 1024}K)")
 
